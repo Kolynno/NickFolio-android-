915c652e7e9fc5fbc140256ed33d01ac1872aa32
Nickolay Zhmakin <nickzhmakin@mail.ru>
2023-11-15 19:18:43 +0300

    add stocks add to portfolio

--- app/src/main/java/itmo/nick/nickfolio/portfolio_operations/PortfolioOperations.kt
+++ app/src/main/java/itmo/nick/nickfolio/portfolio_operations/PortfolioOperations.kt
@@ -1,5 +1,6 @@
 package itmo.nick.nickfolio.portfolio_operations
 
+import android.util.Log
 import itmo.nick.nickfolio.database.OfferDao
 import itmo.nick.nickfolio.database.PortfolioDao
 import kotlinx.coroutines.Dispatchers
@@ -8,15 +9,28 @@ import kotlinx.coroutines.runBlocking
 
 class PortfolioOperations {
     companion object{
-        fun addToPortfolio(portfolioName: String, portfolioRepository: PortfolioDao, offerRepository: OfferDao, offerName: String, ) {
+        fun addToPortfolio(portfolioName: String, portfolioRepository: PortfolioDao, offerRepository: OfferDao, offerName: String) {
             runBlocking {
                 launch(Dispatchers.IO) {
                     val portfolio = portfolioRepository.getPortfolioByName(portfolioName)
-                    val stocksIds = offerRepository.getStocksIdsByName(offerName)
-                    portfolio.stocksIds = stocksIds
+
+                    val currentStocksIds = portfolio.stocksIds?.split(",")?.filter { it.isNotEmpty() }?.toMutableSet()
+
+                    // Получаем новые акции для добавления
+                    val newStocksIds = offerRepository.getStocksIdsByName(offerName).split(",")
+
+                    // Добавляем только уникальные акции
+                    currentStocksIds?.addAll(newStocksIds)
+
+                    val endIds = currentStocksIds?.joinToString(",")
+
+                    // Обновляем портфель с новыми акциями
+                    portfolio.stocksIds = endIds
                     portfolioRepository.update(portfolio)
                 }
             }
         }
+
+
     }
 }
