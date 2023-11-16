591f63d8d7a440e063964b2be17409d76f945126
Nickolay Zhmakin <nickzhmakin@mail.ru>
2023-11-16 08:27:55 +0300

    some nices

--- app/src/main/java/itmo/nick/nickfolio/ui/stock_description/StockDescriptionFragment.kt
+++ app/src/main/java/itmo/nick/nickfolio/ui/stock_description/StockDescriptionFragment.kt
@@ -39,12 +39,18 @@ class StockDescriptionFragment : Fragment() {
         val stockDividends2023 = arguments?.getString("stockDividend2023")
         val stockCurrency = arguments?.getString("stockCurrency")
 
-        binding.stockName.text = "Название акции:$stockName"
-        binding.stockSymbol.text = "Код акции:$stockSymbol"
-        binding.stockSector.text = "Сектор:$stockSector"
-        binding.stockPrice2023.text = "Цена на начало 2023 года:$stockPrice2023"
-        binding.stockDividends2023.text = "Дивиденды за 2023 год:$stockDividends2023"
-        binding.stockCurrency.text = "Валюта акции:$stockCurrency"
+
+        binding.stockName.text = "Название: $stockName"
+        binding.stockSymbol.text = "Код акции: $stockSymbol"
+        binding.stockSector.text = "Сектор: $stockSector"
+        if(stockCurrency == "RUB") {
+            binding.stockPrice2023.text = "Цена на начало 2023 года: $stockPrice2023₽"
+            binding.stockDividends2023.text = "Дивиденды за 2023 год: $stockDividends2023₽"
+        } else {
+            binding.stockPrice2023.text = "Цена на начало 2023 года: $stockPrice2023$"
+            binding.stockDividends2023.text = "Дивиденды за 2023 год: $stockDividends2023$"
+        }
+
 
         (activity as AppCompatActivity).supportActionBar?.title = stockName
         return view
